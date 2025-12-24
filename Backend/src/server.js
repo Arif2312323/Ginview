2c801dac627fafa3bedb6b4f59a8c9b83548ebea
Arif2312323 <arifalmas3.14@gmail.com>
2025-12-24 15:03:24 +0530

    Ready to deploy

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,28 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-
+import path from "path";
 
 
 const app = express();
+const __dirname = path.resolve();
 const port = ENV.PORT;
+const NODE_ENV = ENV.NODE_ENV;
+
 
 console.log(port)
 
-app.get("/",(req,res)=>{
+app.get("/health",(req,res)=>{
     return res.status(200).json({msg:"Api working"});
 })
+
+
+if(NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port,()=>{ 
     console.log("Server running on port");
